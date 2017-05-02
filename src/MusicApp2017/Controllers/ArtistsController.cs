59d4c40b6c4a5fd2011f9c543198f996a6b7f3f0
Devash Anandpara <Devash Anandpara@DESKTOP-NDB0GV0>
2017-05-01 22:03:22 -0400

    Add project files.

---
+++ src/MusicApp2017/Controllers/ArtistsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MusicApp2017.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MusicApp2017.Controllers
+{
+    public class ArtistsController : Controller
+    {
+        private readonly MusicDbContext _context;
+
+        public ArtistsController(MusicDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Artists
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Artists.ToListAsync());
+        }
+
+        
+
+        // GET: Artists/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var albumsContext = _context.Albums.Include(a => a.Artist).Include(a => a.Genre);
+            var albums = await albumsContext.Where(a => a.ArtistID == id).ToListAsync();
+
+            if (albums == null)
+            {
+                return NotFound();
+            }
+
+            return View(albums);
+        }
+
+        // GET: Artists/Create
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Artists/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ArtistID,Name,Bio")] Artist artist)
+        {
+            if (ModelState.IsValid && !this.ArtistExists(artist.Name))
+            {
+                _context.Add(artist);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(artist);
+        }
+
+        // GET: Artists/Edit/5
+        [Authorize]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var artist = await _context.Artists.SingleOrDefaultAsync(m => m.ArtistID == id);
+            if (artist == null)
+            {
+                return NotFound();
+            }
+            return View(artist);
+        }
+
+        // POST: Artists/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ArtistID,Name,Bio")] Artist artist)
+        {
+            if (id != artist.ArtistID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(artist);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArtistExists(artist.ArtistID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(artist);
+        }
+
+        // GET: Artists/Delete/5
+        [Authorize]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var artist = await _context.Artists
+                .SingleOrDefaultAsync(m => m.ArtistID == id);
+            if (artist == null)
+            {
+                return NotFound();
+            }
+
+            return View(artist);
+        }
+
+        // POST: Artists/Delete/5
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var artist = await _context.Artists.SingleOrDefaultAsync(m => m.ArtistID == id);
+            _context.Artists.Remove(artist);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+
+        private bool ArtistExists(int id)
+        {
+            return _context.Artists.Any(e => e.ArtistID == id);
+        }
+
+        private bool ArtistExists(string name)
+        {
+            return _context.Artists.Any(e => e.Name == name);
+        }
+    }
+}
---
+++ src/MusicApp2017/Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicApp2017.Models
+{
+    public class Genre
+    {
+        public int GenreID { get; set; }
+        [Required(ErrorMessage = "Genre name is required")]
+        [StringLength(20, ErrorMessage = "Genre name can't be more than 20 characters")]
+        public string Name { get; set; }
+    }
+}
